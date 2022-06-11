ed0b1110a8909c0ca370519a0f5f871644aeeffb
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-11 15:35:25 +0300

    Create Profile class model

--- projects/models.py
+++ projects/models.py
@@ -5,11 +5,24 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     title = models.CharField(max_length=140)
     user = models.ForeignKey(User, )
-    image = models.ImageField(default='')
+    image = models.ImageField(upload_to = 'projects/', default='')
     description = models.TextField()
     country = models.CharField(max_length=140)
     project_link = models.CharField(max_length=2048)
 
     def __str__(self):
-        return self.title()
+        return self.title
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile' )
+    name = models.CharField(max_length=140)
+    bio = models.TextField()
+    image = models.ImageField(upload_to = 'images/', default='')
+    phone = models.IntegerField(max_length=10)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
 
