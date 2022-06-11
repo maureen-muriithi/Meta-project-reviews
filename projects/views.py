f44d03c35baf9fca960ad1bcd6762fdaed442606
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-11 15:15:08 +0300

    set all configurations to ensure app is running

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
+from re import I
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    message = 'I love Django!!'
+
+    args = {
+        "message": message,
+    }
+
+    return render(request, 'projects/index.html', args)
