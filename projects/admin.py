b62dd4018a8a8c20d810c696ccaabac93caacfbc
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-11 17:19:15 +0300

    Register models in the admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Project
 
 # Register your models here.
+admin.site.site_header = 'Meta-Projects-Reviews - Administration'
+admin.site.register(Profile)
+admin.site.register(Project)
