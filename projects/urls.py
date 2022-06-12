e6784e37c9164bc3cbbf2a81a3dec828d5b6db8c
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-12 18:21:00 +0300

    Add register function and url

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register', views.register_user, name='register'),
 ]
 
 if settings.DEBUG:
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,10 @@ from .models import Project, Profile
 from django.shortcuts import render
 from django.contrib.auth.models import User
 import datetime as dt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -18,4 +22,16 @@ def index(request):
 
     return render(request, 'projects/index.html', args)
 
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+          user = form.save()
+          login(request, user)
+          messages.success(request, "Registration successful." )
+        return redirect("/login")
+    messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = RegisterForm()
+    return render(request=request, template_name="django_registration/registration_form.html", context= {'form': form})
 
