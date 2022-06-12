fbf0950bf15f00fb59c336db2d93dbfa765fc4e5
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-12 18:31:24 +0300

    Add login function

--- projects/views.py
+++ projects/views.py
@@ -36,3 +36,24 @@ def register_user(request):
     form = RegisterForm()
     return render(request=request, template_name="django_registration/registration_form.html", context= {'form': form})
 
+def login_user(request):
+  if request.method == "POST":
+      form = AuthenticationForm(request, data=request.POST)
+      if form.is_valid():
+          username = form.cleaned_data.get('username')
+          password = form.cleaned_data.get('password')
+          user = authenticate(username=username, password=password)
+          if user is not None:
+            login(request, user)
+            messages.info(request, f"You are now logged in as {username}.")
+            return redirect("index")
+          else:
+            messages.error(request,"Invalid username or password.")
+      else:
+          messages.error(request,"Invalid username or password.")
+  form = AuthenticationForm()
+
+  return render(request=request, template_name="registration/login.html", context={"form":form})
+
+
+
