5c4e304a878df5bc2f20c9e619f1b153dd6792be
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-14 15:27:09 +0300

    Add date column and make migrations

--- projects/models.py
+++ projects/models.py
@@ -12,6 +12,7 @@ class Project(models.Model):
     description = models.TextField()
     country = models.CharField(max_length=140)
     project_link = models.URLField(max_length=2048)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
