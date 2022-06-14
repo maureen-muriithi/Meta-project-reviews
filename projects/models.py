09349e89f7b03090dc1a0357ab89fd8cd3a1afca
Maureen-Muriithi <moh2wanja@gmail.com>
2022-06-14 15:36:02 +0300

    Update  migrations

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
 class Project(models.Model):
@@ -12,7 +13,7 @@ class Project(models.Model):
     description = models.TextField()
     country = models.CharField(max_length=140)
     project_link = models.URLField(max_length=2048)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return self.title
